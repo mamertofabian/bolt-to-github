edd0064a4af20e4ab7f7613ea648335f19844134
Mamerto Fabian Jr <mfabianjr@gmail.com>
2024-11-21 09:54:58 +0800

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,30 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.json' assert { type: 'json' };
 
-export default defineConfig({
-  plugins: [
-    svelte(),
-    crx({ manifest }),
-  ],
-  build: {
-    target: 'esnext',
-    emptyOutDir: true,
-    minify: false,
-  }
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      svelte(),
+      crx({ manifest }),
+    ],
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+      minify: mode === 'production',
+      sourcemap: mode === 'development',
+      rollupOptions: {
+        output: {
+          format: 'es',
+          dir: 'dist'
+        }
+      }
+    },
+    server: {
+      port: 5173,
+      strictPort: true,
+      hmr: {
+        port: 5173
+      }
+    }
+  };
 });
